6715a3cfcd11cf079e377d529eb8b0271a8e28b7
Darrin Lowery <lambda@monsto.com>
2021-07-22 17:34:10 +0000

    .

--- services/web/src/routes/about.svelte
+++ services/web/src/routes/about.svelte
@@ -12,32 +12,5 @@
 <p>This is the 'about' page. There's not much here.</p>
 
 <FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>1 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>2 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>3 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>4 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>5 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>6 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>7 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>8 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>9 Some people see this, others don't!</p>
-</FeatureToggle>
-<FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
-	<p>10 Some people see this, others don't!</p>
+	<p>Some people see this, others don't!</p>
 </FeatureToggle>
